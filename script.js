9dffb1b0d774c976613b6b5f02d5bf985d8de1a4
Rachel LeBoeuf <rachelnleboeuf@gmail.com>
2022-02-24 14:44:37 -0500

    js commits

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(#"generate");
+
+
+
+function generatePassword() {
+  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+  
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var genPassword = "";
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * letters.length);
+    genPassword += letters[random];
+  }
+
+  console.log(genPassword);
+
+  return genPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
