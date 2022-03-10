25deb3a34ad657798ca610437d45a7a69f920489
Rachel LeBoeuf <rachelnleboeuf@gmail.com>
2022-03-10 11:56:04 -0500

    added criteria code to js

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# password-generator
+# Password Generator
+
+## Password Generator designed using JavaScript. Will generate random password prompts when given criteria options are chosen from by user. Generator then provides password options with selected criteria. When all prompts have been answered by user, the generator will provide a password that matches the criteria. Then the password is generated for the user. 
+
+
+
+
+
+
+
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-var generateBtn = document.querySelector(#"generate");
+var generateBtn = document.querySelector("#generate");
 
-
+// Add length console logs (or functions ?) for message generators. One for length of the password-
+// (8 characters and no more than 128 characters), One for choosing between lowercase,
+// uppercase, numeric,  and/or special characters, one that validates the criteria, and 
+// one that generates the password prompts then options. 
 
 function generatePassword() {
-  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
-  
   var letters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special = "!@#$%^&*()_+~";
+  var numbers = "0123456789"
+  var count = parseInt(prompt('Please choose a password of at least 8 characters and up to 128 characters'));
+  if (count < 8 || count > 128 || isNaN(count)) {
+    alert('Please choose a value between 8 characters and 128 characters to move on');
+    return generatePassword()
+  }
+  var hasLetters = confirm('Do you want to use lowercase Characters?');
+  var hasUpperLetters = confirm('Do you want to use uppercase Characters?');
+  var hasSpecialLetters = confirm('Do you want to use special Characters?');// fix grammar here ***
+  var hasNumbers = confirm('Do you want to use Numbers?');
+  var possibleCharacters = '';
+
+
+  if (!hasLetters && !hasUpperLetters && !hasSpecialLetters && !hasNumbers) {
+    alert('Please choose confirm for one or more of the criteria options');
+    return generatePassword()
+  }
+
+  if (hasLetters) {
+
+    possibleCharacters += letters;
+
+  }
+
+  if (hasUpperLetters) {
+
+    possibleCharacters += uppercase;
+  }
+
+  if (hasSpecialLetters) {
+
+    possibleCharacters += special
+  }
+
+  if (hasNumbers) {
+
+    possibleCharacters += numbers
+  }
+
   var genPassword = "";
 
+
   for (var i = 0; i < count; i++) {
-    var random = Math.floor(Math.random() * letters.length);
-    genPassword += letters[random];
+    var random = Math.floor(Math.random() * possibleCharacters.length);
+    genPassword += possibleCharacters[random];
   }
 
   console.log(genPassword);
@@ -20,10 +63,10 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var genPassword = generatePassword();
+  var genPasswordText = document.querySelector("#genPassword");
 
-  passwordText.value = password;
+  genPasswordText.value = genPassword;
 
 }
 
